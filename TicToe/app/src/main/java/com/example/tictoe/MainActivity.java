9cb9376cd898c18faccd9ad604576270e4eba71d
DenzelAlexander <denzel.a.alexander@gmail.com>
2020-02-06 17:57:06 -0800

    Initial commit of finished game
    game is working with simple A.I.
    One glitch is when clicking an area that has already been placed the game will play by itselfs

---
+++ TicToe/app/src/main/java/com/example/tictoe/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.tictoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BoardView boardView;
+    private GameEngine gameEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boardView = (BoardView) findViewById(R.id.board);
+        gameEngine = new GameEngine();
+        boardView.setGameEngine(gameEngine);
+        boardView.setMainActivity(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_new_game) {
+            newGame();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void gameEnded(char c) {
+        String msg = (c == 'T') ? "Game Ended. Tie" : "GameEnded. " + c + "win";
+
+        new AlertDialog.Builder(this).setTitle("Tic Tac Toe").
+                setMessage(msg).
+                setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        newGame();
+                    }
+                }).show();
+
+    }
+
+    private void newGame() {
+        gameEngine.newGame();
+        boardView.invalidate();
+    }
+    public void createAlertDialog() {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Alert message to be shown");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+}
